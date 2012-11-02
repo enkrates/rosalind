51cfba8d31a1b7af13f5f4cd1503848e163ae03b
William Sullivan <enkrates@gmail.com>
2012-11-02 11:59:17 -0500

    Fixed awful formatting in DNA code

--- DNA/ruby/main.rb
+++ DNA/ruby/main.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby -wKU
 
 if ARGV[0]
-	data_file = '../data/' + ARGV[0]
+  data_file = '../data/' + ARGV[0]
 else
-	data_file = "../data/sample.txt"
+  data_file = "../data/sample.txt"
 end
 
 bases = Hash["A" => 0,
-						 "C" => 0,
-						 "G" => 0,
-				 		 "T" => 0]
+             "C" => 0,
+             "G" => 0,
+             "T" => 0]
 
 File.open(data_file, 'r').each_char do |character|
-	bases[character] += 1
+  bases[character] += 1
 end
 
 puts bases.values.join(' ')
