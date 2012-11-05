1a3b1b7bf3c367c179485f0f45a3c654621e9b11
William Sullivan <enkrates@gmail.com>
2012-11-05 13:57:31 -0600

    Solved PROB with some math help from my mathemagician wife

---
+++ PROB/ruby/main.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby -wKU
+
+if ARGV[0]
+  data_file = '../data/' + ARGV[0]
+else
+  data_file = "../data/sample.txt"
+end
+
+data_array = File.open(data_file, 'r').read.split(" ")
+
+final_result_array = Array.new
+
+data_array.each do |datum|
+  final_result_array << ( (((datum.to_f/2)**2)*2) + 
+                          ((((1.0 - datum.to_f)/2)**2)*2) ).round(4)
+end
+
+puts final_result_array.join(" ")
