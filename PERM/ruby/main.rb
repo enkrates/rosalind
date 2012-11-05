10a40230328079260b23242dab68b33c7ab4cfb3
William Sullivan <enkrates@gmail.com>
2012-11-05 13:12:12 -0600

    Prettying up PERM

--- PERM/ruby/main.rb
+++ PERM/ruby/main.rb
@@ -5,19 +5,19 @@ length = ARGV[0]
 original_string = (1..length.to_i).to_a.join
 
 def permutations string
-	list_permutations "", string	
+  list_permutations "", string	
 end
 
 def list_permutations prefix, rest
-	if rest.length == 0
-		puts prefix.split("").join(" ")
-	else
-	  rest.each_char do |c|
+  if rest.length == 0
+    puts prefix.split("").join(" ")
+  else
+    rest.each_char do |c|
       pre = prefix + c
       remaining = rest.delete(c)
       list_permutations pre, remaining
-		end
-	end
+    end
+  end
 end
 
 def factorial num
