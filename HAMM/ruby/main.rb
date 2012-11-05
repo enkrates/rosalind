c0a544d15bddee7a73be9bb11f7396fb07feb531
William Sullivan <enkrates@gmail.com>
2012-11-04 20:52:47 -0600

    Solved HAMM

---
+++ HAMM/ruby/main.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby -wKU
+
+if ARGV[0]
+  data_file = '../data/' + ARGV[0]
+else
+  data_file = "../data/sample.txt"
+end
+
+data_array = File.open(data_file, 'r').read.split("\n")
+
+hamming = 0
+
+(0...(data_array[0].size)).each do |index|
+	hamming += ((data_array[0][index] == data_array[1][index]) ? 0 : 1)
+end
+
+puts hamming
