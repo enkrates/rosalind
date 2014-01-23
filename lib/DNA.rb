1e0743de0245c3a4a51b11ae1f1285801c65734b
William Sullivan <william@enkrates.com>
2014-01-23 15:20:23 -0600

    Small formatting fix for DNA

--- lib/DNA.rb
+++ lib/DNA.rb
@@ -8,14 +8,14 @@ class DNA
  
   def nucleobase_count
   	bases = Hash["A" => 0,
-	             "C" => 0,
-	             "G" => 0,
-	             "T" => 0]
+  	             "C" => 0,
+  	             "G" => 0,
+  	             "T" => 0]
 
-	@dna_string.each_char do |character|
-	  bases[character] += 1
-	end
-    bases.values
+  	@dna_string.each_char do |character|
+  	  bases[character] += 1
+  	end
+      bases.values
   end
  
 end
